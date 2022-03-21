3015b4a1394dccb2ef0d13c78316ab984e483067
YuriMayk <yuri.mayk@gmail.com>
2022-03-20 21:56:39 -0300

    Sarted to develop the logic.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   Container,
   Image,
@@ -11,13 +11,25 @@ import {
 } from "./styles";
 import MainImage from "./assets/image-login.svg";
 import ArrowImage from "./assets/arrow.svg";
-import Trash from "./assets/trash.svg"
+import Trash from "./assets/trash.svg";
 
 const App = () => {
-  const users = [
-    { id: Math.random(), name: "Yuri", age: 25 },
-    { id: Math.random(), name: "Vilton", age: 18 },
-  ];
+  const [users, setUsers] = useState([]);
+  const InputName = useRef();
+  const InputAge = useRef();
+  
+
+  function addNewUser() {
+    setUsers([
+      ...users,
+      {
+        id: Math.random(),
+        name: InputName.current.value,
+        age: InputAge.current.value,
+      },
+    ]);
+    console.log(InputName.current.value, InputAge.current.value);
+  }
 
   return (
     <Container>
@@ -26,18 +38,21 @@ const App = () => {
         <H1>Olá!</H1>
 
         <Paragraph>Nome</Paragraph>
-        <Input placeholder="Seu nome"></Input>
+        <Input ref={InputName} placeholder="Seu nome"></Input>
 
         <Paragraph>Idade</Paragraph>
-        <Input placeholder="Sua idade"></Input>
+        <Input ref={InputAge} placeholder="Sua idade"></Input>
 
-        <Button>
+        <Button onClick={addNewUser}>
           Cadastrar <img alt="arrow" src={ArrowImage} />
         </Button>
         <ul>
           {users.map((user) => (
             <User key={user.id}>
-              <p>{user.name}</p> - <p>{user.age}</p> <button> <img alt="trash" src={Trash} /></button>
+              <p>{user.name}</p> - <p>{user.age}</p>{" "}
+              <button onClick={deleteUser}>
+                <img alt="trash" src={Trash} />
+              </button>
             </User>
           ))}
         </ul>
